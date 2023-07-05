75a466d580d00e2b90624a81f33b82d076483677
JackLash09 <jacquesboudreau01@gmail.com>
2023-07-05 12:17:06 +0200

    added 2.2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,16 +1,20 @@
+import React, { useState } from 'react';
+
 const Course = ({course}) => <h1>{course}</h1>
 
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <p>total of {sum} exercises</p>
+
 
 const Content = ({id, name, exercises }) => {
   return(
-  <p key={id}>
-    {name} {exercises}
-  </p>
+    <p key={id}>
+      {name} {exercises}
+    </p>
   )
 }
+
 
 const App = () => {
   const course = {
@@ -31,19 +35,28 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
+  const sum = course.parts.reduce((total, currentValue) => total = total + currentValue.exercises,0);
+
   return (
     <div>
       <Course course={course.name} />
-      {course.parts.map((part) => {
-        return(
-          <Content id={part.id} name={part.name} exercises={part.exercises} />
-        );
-      })}
-      <Content parts={course.parts} /> 
+      <>
+        {course.parts.map((part) => {
+          return(
+            <Content id={part.id} name={part.name} exercises={part.exercises} />
+          );
+        })}
+      </>
+      <Total sum={sum}/>
     </div>
   )
 }
