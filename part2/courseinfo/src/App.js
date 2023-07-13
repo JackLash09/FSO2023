e165a37034806947ef01a0f004ac92e95efa1788
JackLash09 <jacquesboudreau01@gmail.com>
2023-07-13 14:04:38 +0200

    added 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import courses from './Data'
 
 const Course = ({id, name }) => {
   return(
@@ -21,50 +22,7 @@ const Content = ({ id, name, exercises }) => {
 
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+
   let sum = 0
   /*
   const sum = course.parts.reduce((total, currentValue) => total = total + currentValue.exercises, 0);
