366f00449d2002c22b922f68d1019b5a232517f7
JackLash09 <jacquesboudreau01@gmail.com>
2023-07-13 13:57:28 +0200

    added 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
-const Course = ({course}) => <h1>{course}</h1>
-
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>total of {sum} exercises</p>
-
-
-const Content = ({id, name, exercises }) => {
+const Course = ({id, name }) => {
   return(
+    <h3 key={id}>{name}</h3>
+  )
+}
+
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Total = ({ sum }) => <h4>total of {sum} exercises</h4>
+
+
+const Content = ({ id, name, exercises }) => {
+  return (
     <p key={id}>
       {name} {exercises}
     </p>
@@ -17,47 +21,81 @@ const Content = ({id, name, exercises }) => {
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
-  const sum = course.parts.reduce((total, currentValue) => total = total + currentValue.exercises,0);
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  let sum = 0
+  /*
+  const sum = course.parts.reduce((total, currentValue) => total = total + currentValue.exercises, 0);
+  */
+ 
   return (
-    <div>
-      <Course course={course.name} />
-      <>
-        {course.parts.map((part) => {
-          return(
-            <Content id={part.id} name={part.name} exercises={part.exercises} />
-          );
-        })}
-      </>
-      <Total sum={sum}/>
-    </div>
+    <>
+      <h1>
+        Web development curriculum
+      </h1>
+      {courses.map((course) => {
+        sum = 0
+        return(
+          <div>
+            <div>
+              <Course id={course.id} name={course.name}/>
+            </div>
+            {
+              course.parts.map((part) => (
+                sum = sum + part.exercises,
+                <Content id={part.id} name={part.name} exercises={part.exercises}/>
+              ))
+            }
+            <div>
+              <Total sum={sum}/>
+            </div>
+          </div>
+        )
+      }
+      )}
+    </>
   )
 }
 
