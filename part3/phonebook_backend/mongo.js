1eec33b5ed4f6945f168341852b4ec061d6a08a4
JackLash09 <jacquesboudreau01@gmail.com>
2023-08-22 15:02:42 +0200

    added 3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@myclouddb.cu4nksi.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+}
+else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      })
+      
+      person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+      })
+}
+else {
+    console.log(process.argv.length)
+    console.log('follow current format:node mongo.js password "firstName lastName" number')
+    process.exit(1)
+}
