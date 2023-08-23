45bdfc16af2a6acf1c614f0cad2580210e2e8639
JackLash09 <jacquesboudreau01@gmail.com>
2023-08-23 13:57:05 +0200

    added 3.16 - 3.18

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -34,24 +34,26 @@ app.get('/info', (request, response) => {
     })
   })
 
-
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
-    console.log(body.id)
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    person.update().then(updatedPerson => {
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
       response.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -77,6 +79,18 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
